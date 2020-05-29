f99d7923b7aa56cc5867273982a5809681af006d
Kevin Cunningham <kevin@kevincunningham.co.uk>
2020-05-29 16:11:51 +0100

    end-of-lesson-3

--- api/server.js
+++ api/server.js
@@ -5,11 +5,30 @@ const cors = require("cors");
 const app = express();
 const imageDirectory = "/public";
 const BASE_URL = "http://localhost:3001";
+const answers = require("./answers.json");
 
 app.use("/", express.static("public"));
 app.use(cors());
+app.use(express.json());
 
 app.get("/images", (req, res) => res.json({ src: selectRandomFile() }));
+
+app.post("/guess", (req, res) => {
+  const imageName = req.body.image.substring(
+    req.body.image.lastIndexOf("/") + 1
+  );
+  const answer = answers.filter((item) => item.image === imageName)[0].answer;
+  const result =
+    answer.toLowerCase().replace(/ /g, "") ===
+    req.body.guess.toLowerCase().replace(/ /g, "");
+
+  const message = result
+    ? "Well done! Click for a new image."
+    : "Not this time. Check your spelling and try again.";
+
+  res.json({ msg: message });
+});
+
 app.listen(3001, function () {
   console.log("Server started.");
 });
--- app/pages/index.js
+++ app/pages/index.js
@@ -4,15 +4,20 @@ import { client } from "../utils/client";
 export default function index() {
   const [imgSrc, setImgSrc] = React.useState("");
   const [guess, setGuess] = React.useState("");
+  const [result, setResult] = React.useState("");
 
   const fetchNewImage = async () => {
     const path = await client("images").then((data) => data.src);
     setImgSrc(path);
+    setGuess("");
+    setResult("");
   };
 
-  const submitGuess = () => {
-    setGuess("");
-    console.log(guess);
+  const submitGuess = async () => {
+    const response = await client("guess", {
+      body: { image: imgSrc, guess },
+    }).then((data) => data);
+    setResult(response.msg);
   };
 
   return (
@@ -33,6 +38,9 @@ export default function index() {
             onChange={(e) => setGuess(e.target.value)}
           />
           <button onClick={submitGuess}>Submit Guess</button>
+          <div>
+            {result && <div style={{ textAlign: "center" }}>{result}</div>}
+          </div>
         </div>
       </div>
     </div>
