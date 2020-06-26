e54aa14831adcfd49718ef9d6c8233f8a3574647
Kevin Cunningham <kevin@kevincunningham.co.uk>
2020-06-26 18:56:59 +0100

    End of the intro lesson.

--- api/server.js
+++ api/server.js
@@ -1,21 +1,11 @@
 const express = require("express");
-const fs = require("fs");
 const cors = require("cors");
 
 const app = express();
-const imageDirectory = "/public";
-const BASE_URL = "http://localhost:3001";
 
 app.use("/", express.static("public"));
-app.use(cors());
 
-app.get("/images", (req, res) => res.json({ src: selectRandomFile() }));
+app.get("/", (req, res) => res.send("this is working"));
 app.listen(3001, function () {
   console.log("Server started.");
 });
-
-function selectRandomFile() {
-  const files = fs.readdirSync(__dirname + imageDirectory);
-  const chosenFile = files[Math.floor(Math.random() * files.length)];
-  return `${BASE_URL}/${chosenFile}`;
-}
