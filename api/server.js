9e7a1bf008fdd7bd6536bb854d1eb789ee4ef456
Kevin Cunningham <kevin@kevincunningham.co.uk>
2020-09-17 13:44:54 +0100

    JWT authorization.

--- api/server.js
+++ api/server.js
@@ -1,13 +1,37 @@
 const express = require("express");
+const request = require("request");
+require("dotenv").config();
+
 const app = express();
-require("express-async-errors");
-const { errorHandling } = require("./utils/error");
+const {checkJwt} = require("./utils/auth");
 
 app.get("/test", async (req, res) => {
-  throw new Error("Oh no! The world has ended!");
+  res.send("This is a test.")
 });
 
-app.use(errorHandling);
+app.get("/test-auth", checkJwt, async (req, res) => {
+  res.send("This is a test.")
+});
+
+app.get("/get-token", (req, res) => {
+  request({
+    method: "POST",
+    url: "https://dev-signup.eu.auth0.com/oauth/token",
+    headers: {"content-type": "application/x-www-form-urlencoded"},
+    form: {
+      grant_type: "client_credentials",
+      client_id: process.env.AUTH0_CLIENT_ID,
+      client_secret: process.env.AUTH0_CLIENT_SECRET,
+      audience: "https://catchphrase.app"
+    }
+  }, function(error, response, body) {
+    if (error) {
+      res.status(400);
+      res.send(error);
+    }
+    res.send(body);
+  })
+})
 
 app.listen(3001, function () {
   console.log("Server started.");
--- api/utils/auth.js
+++ api/utils/auth.js
@@ -1,13 +1,15 @@
-function auth(req, res, next) {
-  // x-api-key
-  // if not x-api-key header
-  // if not right key
-  if (!req.header("x-api-key") || req.header("x-api-key") !== "test-api-key") {
-    res.status(401)
-    return res.json({ message: "Invalid API key" })
-  }
+const jwt = require("express-jwt");
+const jwksRsa = require("jwks-rsa");
 
-  next()
-}
+const checkJwt = jwt({
+  secret: jwksRsa.expressJwtSecret({
+      cache: true,
+      rateLimit: true,      jwksRequestsPerMinute: 5,
+      jwksUri: 'https://dolearning-egghead.eu.auth0.com/.well-known/jwks.json'
+}),
+audience: 'https://catchphrase.app',
+issuer: 'https://dolearning-egghead.eu.auth0.com/',
+algorithms: ['RS256']
+});
 
-module.exports = { auth }
+module.exports = { checkJwt };
